7c804e6870f91f8a32d305b22b1d795cdc701920
Stephen Fortune <steve.fortune@icecb.com>
2014-04-05 23:13:50 +0100

    #11 Setup empty Example Case 1 test

---
+++ Test App/Test AppTests/I3Example1Test.h
@@ -0,0 +1,14 @@
+//
+//  I3Example1Test.h
+//  Test App
+//
+//  Created by Stephen Fortune on 05/04/2014.
+//  Copyright (c) 2014 IceCube Software Ltd. All rights reserved.
+//
+
+#import "KIFTestCase.h"
+#import "KIFUITestActor+I3DndTestAppAdditions.h"
+
+@interface I3Example1Test : KIFTestCase
+
+@end
---
+++ Test App/Test AppTests/I3Example1Test.m
@@ -0,0 +1,59 @@
+//
+//  I3Example1Test.m
+//  Test App
+//
+//  Created by Stephen Fortune on 05/04/2014.
+//  Copyright (c) 2014 IceCube Software Ltd. All rights reserved.
+//
+
+#import "I3Example1Test.h"
+
+@implementation I3Example1Test
+
+
+-(void) beforeEach{
+    
+    [tester navigateToExampleCaseNumber:1];
+    
+}
+
+-(void) afterEach{
+    
+}
+
+-(void) testSrcIsRearrangeable{
+
+}
+
+-(void) testDstIsRearrangeable{
+
+}
+
+-(void) testSrcCannotDragToDst{
+
+}
+
+-(void) testDstCannotDragToSrc{
+
+}
+
+-(void) testSrcCellsCannotBeDraggedOustideOfTable{
+
+}
+
+-(void) testAllCellsAreDraggable{
+
+}
+
+-(void) testAllCellsAreRearrangeable{
+
+}
+
+-(void) testNoCellsAreExchangeable{
+}
+
+-(void) testDstCellsCannotBeDraggedOustideOfTable{
+    
+}
+
+@end
