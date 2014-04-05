efeabc7bfc67f4fc5630238b90127139255e8949
Stephen Fortune <steve.fortune@icecb.com>
2014-04-05 21:43:03 +0100

    #11 Added empty kif ui actor category that will provide methods for mavigating between example cases

---
+++ Test App/Test AppTests/KIFUITestActor+I3DndTestAppAdditions.h
@@ -0,0 +1,26 @@
+//
+//  KIFUITestActor+I3DndTestAppAdditions.h
+//  Test App
+//
+//  Created by Stephen Fortune on 05/04/2014.
+//  Copyright (c) 2014 IceCube Software Ltd. All rights reserved.
+//
+
+#import "KIFUITestActor.h"
+
+@interface KIFUITestActor (I3DndTestAppAdditions)
+
+/**
+ *  Helper additional methods for navigating between the current test 
+ *  cases.
+ */
+
+-(void) navigateToExampleCase1;
+-(void) navigateToExampleCase2;
+-(void) navigateToExampleCase3;
+-(void) navigateToExampleCase4;
+-(void) navigateToExampleCase5;
+-(void) navigateToExampleCase6;
+-(void) navigateToExampleCase7;
+
+@end
---
+++ Test App/Test AppTests/KIFUITestActor+I3DndTestAppAdditions.m
@@ -0,0 +1,51 @@
+//
+//  KIFUITestActor+I3DndTestAppAdditions.m
+//  Test App
+//
+//  Created by Stephen Fortune on 05/04/2014.
+//  Copyright (c) 2014 IceCube Software Ltd. All rights reserved.
+//
+
+#import "KIFUITestActor+I3DndTestAppAdditions.h"
+
+@implementation KIFUITestActor (I3DndTestAppAdditions)
+
+
+
+-(void) navigateToExampleCase1{
+
+}
+
+
+-(void) navigateToExampleCase2{
+    
+}
+
+
+-(void) navigateToExampleCase3{
+    
+}
+
+
+-(void) navigateToExampleCase4{
+    
+}
+
+
+-(void) navigateToExampleCase5{
+    
+}
+
+
+-(void) navigateToExampleCase6{
+    
+}
+
+
+-(void) navigateToExampleCase7{
+    
+}
+
+
+
+@end
